630b898a9de6e4035997b239b6ef6d6c9de56aec
LJAEH <wogurdlek33@gmail.com>
2023-02-20 15:13:57 +0900

    프로필창

---
+++ 03_자바스크립트/js/01_개요.js
@@ -0,0 +1,20 @@
+// 한줄 주석
+
+/*
+범 위 주 석
+*/
+
+// js파일은 <script> 태그 내부라고 생각하면댐
+
+function btnClick2() {
+    alert("external 버튼이 클릭ㅇ")
+}
+
+function changeColor1() {
+    document.getElementById("box").style.backgroundColor="red";
+}
+
+
+function changeColor2() {
+    document.getElementById("box").style.backgroundColor="yellow";
+}
